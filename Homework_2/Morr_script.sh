2167bc382bce60733c93c6fc21b15611c1067660
allozavrr <63748214+allozavrr@users.noreply.github.com>
2021-09-13 15:45:36 +0300

    Update Morr_script.sh

--- Homework_2/Morr_script.sh
+++ Homework_2/Morr_script.sh
@@ -8,7 +8,7 @@ read -p "Choose your fighter! What is your process name or PID? " process_name
 #You need type lines to see the process name, Nerevarine!
 
 process_name="${process_name,,}"
-read -p "Write the required number of lines for output (0-99): " lines
+read -p "Write the required number of lines for output (0-99): " lines #you can write lines you need, Traveler!
 reg='^[0-9]{1,2}$' 
 while [[ ! $lines =~ $reg ]]; do
   echo "The request is incorrect, please drink your skuuma and try again (0-99): "
@@ -18,7 +18,7 @@ done
 #Check your connections before starting, Fighter!
 
 while true; do
-  read -p "Enable viewing additional connection states? (YES/NO)" yn
+  read -p "Enable viewing additional connection states? (YES/NO)" yn #you can wiew your connections, Traveler!
   case $yn in
   [Yy]*)
     add_states="y"
@@ -56,12 +56,13 @@ whois_filter="^Organization" #your guild, Traveler!
 #Check your states of ports before starting, Fighter!
 
 function show_add_info {
-  echo "Traveler, you can find other connection states in the Elder Scroll below:"
+  echo "Traveler, you can find other connection states in the Elder Scroll below:" #you can wiew other connections, Traveler!
   netstat -tunapl | awk '/'$process_name'/ ''{print $5 "             " $6}'
   echo -e "_________________________________________________________________\n"
 }
 
 #Show your whois before starting, Fighter!
+#you can wiew it in 5 lines... but as M'Aik the Liar says, this is not accurate
 
 function show_whois {
   if [[ "$run_as_admin" == "y" ]]; then
@@ -71,7 +72,7 @@ function show_whois {
         awk -F':' '/'$whois_filter'/ ''{print $2}'; done | uniq -c
   else
     netstat -tunapF | awk '/'$process_name'/ ''{print $5}' |
-      cut -d: -f1 | sort | uniq -c | sort | tail -n"$lines" | grep -oP '(\d+\.){3}\d+' | while read IP; do whois $IP |
+      cut -d: -f1 | sort | uniq -c | sort | tail -n"$lines" | grep -oP '(\d+\.){3}\d+' | while read IP; do whois $IP | 
         awk -F':' '/'$whois_filter'/ ''{print $2}'; done | uniq -c
   fi
 }
@@ -106,4 +107,6 @@ if [[ "$run_again" == "y" ]]; then
   show_whois 
 fi 
 
+# Completed the Dwemer Ruins of Dagoth Ur. So this was pretty confusing. But for you it was not difficult.
 echo "Morrowind thanks you, Nerevarine!"
+
