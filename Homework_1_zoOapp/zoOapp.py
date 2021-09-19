c2d82a3665274baa029004987f17e6a37296c79a
allozavrr <63748214+allozavrr@users.noreply.github.com>
2021-09-20 00:34:12 +0300

    Update zoOapp.py

--- Homework_1_zoOapp/zoOapp.py
+++ Homework_1_zoOapp/zoOapp.py
@@ -1,1 +1,49 @@
+import json
+import emoji
+import qrcode
 
+# our file (you are welcome!)
+data = "https://youtu.be/XqZsoesa55w/"
+
+# our file name
+filename = "zoOapp.png"
+
+# go to link
+img = qrcode.make(data)
+
+# save img to file
+img.save(filename)
+
+from flask import Flask, request
+
+app = Flask(__name__)
+	
+# accept GET & POST methods
+@app.route("/", methods=['GET', 'POST'])
+
+def json_example():
+
+	# receive JSON
+	content = request.get_json(force=True
+    
+	# let emoji support
+	animal = content["animal"]
+	sound = content["sound"]
+	count = str(content["count"])
+	emoji =  {'elephant':'\N{elephant}', 'cow':'\N{cow face}', 'whale':'\N{Whale}'}
+	emoji_icon = emoji.get(animal)
+
+	# let's go fun
+	if emoji_icon is None:
+		return  "\nfilename\n"
+	i=0
+	res=''
+	while i<int(count):
+		temp = emoji_icon + animal + " says  "  +  sound + "\n"
+		res =  temp + res
+		i=i + 1
+	res = "\n" + res + "\n Made with by \u1F996 Allozavrr \u1F996\n"
+	return  res
+
+if __name__ == '__main__':
+app.run(host='0.0.0.0', port = 80, ssl_context = ('cert.pem', 'key.pem'))
